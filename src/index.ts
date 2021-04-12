aa96d99855621de90f24cde8e17af9dfe9fe1d0c
pratikg17 <pjgawand@gmail.com>
2021-04-12 11:51:43 +0530

    Setup Mongodb

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
